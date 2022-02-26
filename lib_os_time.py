d6a5117421dd67093ee33883ddffcc69a99b2edd
Kamil_M <kamil.musial@chorzow.wsb.pl>
2022-02-26 11:34:02 +0100

    lib_os_time_finished

--- lib_os_time.py
+++ lib_os_time.py
@@ -4,7 +4,13 @@ import time
 print(os.getcwd())
 os.chdir('C:\\Users\\vdi-belfer\\Desktop\\folder')
 os.mkdir('new_folder')
-time.sleep(3)
+time.sleep(1)
 os.rename('new_folder', 'new_folder2')
-time.sleep(3)
+time.sleep(1)
 os.rmdir('new_folder2')
+
+#znaleźć plik new.txt w danej lokalizacji
+os.system('cmd /c "cd C:\\Users\\vdi-belfer\\Desktop\\folder && dir new.txt >> results.txt"')
+#&&
+#dir new.txt
+#  >> result.txt
